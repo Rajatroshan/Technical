980e46d5ab21ed60252dec5dbb11191ef24cb483
Rajatroshan <rajatroshan2002@gmail.com>
2024-02-19 16:24:49 +0530

    Third commit

--- Day-2/outer.py
+++ Day-2/outer.py
@@ -1,6 +1,13 @@
 def outer():
     name="hello"
     
+    # def inner(name):
+    #     print(name)
+    #     name="goo"
+    #     return name
+    
+    # name=inner(name)
+
     def inner():
         nonlocal name
         print(name)
